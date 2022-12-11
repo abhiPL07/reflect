60150f7af80b7e3046da128246aead4fd608b097
abhiPL07 <abhiPL07@github.com>
2022-12-11 20:16:04 +0530

    implementation from online tutorial

--- reflect-one.go
+++ reflect-one.go
@@ -1,55 +1,55 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"reflect"
+	"strconv"
+	"strings"
 )
 
-type Marks struct {
-	Physics float64
-	Maths   float64
-}
-
 type Person struct {
-	Name   string
-	Age    int
-	Origin struct {
-		Country string
-		City    string
-	}
+	Name string
+	Age  int64
 }
 
 func main() {
-	stud1 := Marks{
-		Physics: 63,
-		Maths:   91,
-	}
-	p1 := Person{
-		Name: "Pal",
+	var p Person = Person{
+		Name: "Lal",
 		Age:  30,
-		Origin: struct {
-			Country string
-			City    string
-		}{
-			Country: "India",
-			City:    "Kolkata",
-		},
 	}
-	reflectItems(stud1)
-	reflectItems(p1)
+	res, err := JsonEncode(p)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(res))
 }
 
-func reflectItems(it interface{}) error {
-	typ := reflect.TypeOf(it)
-	if typ.Kind() != reflect.Struct {
-		fmt.Println("Not the expected value!")
+func JsonEncode(v interface{}) ([]byte, error) {
+	refObjVal := reflect.ValueOf(v)
+	refObjTyp := reflect.TypeOf(v)
+	buf := bytes.Buffer{}
+	if refObjVal.Kind() != reflect.Struct {
+		return buf.Bytes(), fmt.Errorf("unsupported type: %v", refObjVal.Kind())
 	}
-	fields := typ.NumField()
-	fmt.Printf("Type : %v\nValue : %v\n", typ, fields)
-	for i := 0; i < fields; i++ {
-		tt := typ.Field(i)
-		fmt.Printf("#: %v,\tName: %v,\tType: %v\n", i, tt.Name, tt.Type)
+	pairs := []string{}
+	for i := 0; i < refObjVal.NumField(); i++ {
+		fieldVal := refObjVal.Field(i)
+		fieldTyp := refObjTyp.Field(i)
+		switch fieldVal.Kind() {
+		case reflect.String:
+			strVal := fieldVal.Interface().(string)
+			pairs = append(pairs, `"`+string(fieldTyp.Name)+`": "`+strVal+`"`)
+		case reflect.Int64:
+			intVal := fieldVal.Interface().(int64)
+			// pairs = append(pairs, `"`+string(fieldTyp.Name)+`": `+strconv.FormatInt(intVal, 10)+`"`)
+			pairs = append(pairs, fmt.Sprintf("%s: %s", string(fieldTyp.Name), strconv.FormatInt(intVal, 10)))
+		default:
+			return buf.Bytes(), fmt.Errorf("unsupported type: %v", fieldVal.Kind())
+		}
 	}
-	fmt.Println("________Next_________")
-	return nil
+	buf.WriteString("{")
+	buf.WriteString(strings.Join(pairs, " "))
+	buf.WriteString("}")
+	return buf.Bytes(), nil
 }
